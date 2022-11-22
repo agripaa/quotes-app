229a22ef458fbc00709e22b0986682233ac55f94
BangOns <syahr9950@gmail.com>
2022-11-22 16:17:56 +0700

    tolong bikin databese nya dulu mas jon belum ke ranah situ ;(

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-
+import axios from "axios";
 function App() {
-  return (
-    <div className="App">
-      <h1>aman</h1>
-      <h1>halo</h1>
-    </div>
-  );
+  const [quotes, setQuote] = useState([]);
+  useEffect(() => {
+    GetQuotes();
+  }, []);
+  async function GetQuotes() {
+    let res = await axios.get("http://localhost:5000/quote");
+    setQuote(res.data.result);
+  }
+  console.log(quotes);
+  return <div className="App"></div>;
 }
 
 export default App;
