45d3b3d39a60cba5be336b386f9b235aa66a5d51
Jounnyy <agrieva.xananda08@gmail.com>
2022-11-22 08:35:13 +0700

    add frontend folder and get data with axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const [quote, setQuote] = useState([]);
-
-  useEffect(() => {
-    getQuotes();
-  },[])  
-
-  async function getQuotes(){
-    const res = await axios.get('http://localhost:5000/quote');
-    setQuote(res.data.result);
-  }
-
-  console.log({datas: quote})
-
   return (
     <div className="App">
-      
+      a
     </div>
   );
 }
