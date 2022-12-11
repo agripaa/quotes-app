ed37ca355effdcbe02b7f4933d5bcad471809f0e
BangOns <syahr9950@gmail.com>
2022-12-11 15:28:16 +0700

    New Update

--- frontend/src/components/form/form.jsx
+++ frontend/src/components/form/form.jsx
@@ -4,65 +4,173 @@ import "./from.css";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
 import { useEffect } from "react";
+import { Fragment } from "react";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import axios from "axios";
-function TextControlsExample() {
+import NewQuotes from "../Cards/NewQuotes/userCardsNew";
+import WithHeaderAndQuoteExample from "../Cards/userCard";
+import Slider from "../footerCard/footerCard";
+function FormAndQuotes() {
   const [user, setUser] = useState({});
   const [nama, setNama] = useState("");
-  const [quotes, setQuotes] = useState("");
+  const [text, setText] = useState("");
+  const [Data, setData] = useState([]);
 
-  let dataUser = {
-    id: 1,
-    user: "",
-    quote: "",
-  };
+  const [width, setWidth] = useState(window.innerWidth);
+  function handleWidth() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleWidth);
+  }, [width]);
+
   function addQuotes(e) {
     e.preventDefault();
     let date = new Date().getTime();
-    let newData = { ...dataUser };
-    newData["id"] = date;
-    newData.user = nama;
-    newData.quote = quotes;
+    let newData = {
+      id: date,
+      quote: text,
+      user: nama,
+    };
+    console.log(newData);
     setUser(newData);
     setNama("");
-    setQuotes("");
+    setText("");
   }
-
+  async function getNewData() {
+    let res = await axios.get("http://localhost:5000/quote");
+    setData(res.data.result);
+  }
+  async function getPostUser() {
+    let response = await axios.post("http://localhost:5000/quote", user);
+    await getNewData();
+    return response;
+  }
   useEffect(() => {
     document.title = "Quotes App";
-    if (user.user && user.qoute) {
-      axios.post("http://localhost:5000/quote", user).then((v) => {
-        console.log(v);
-      });
+    if (user.user && user.quote) {
+      getPostUser();
     }
   }, [user]);
   return (
-    <Form className="form-user">
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>User</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="your name"
-          className="inputName"
-          value={nama}
-          onChange={(e) => setNama(e.target.value)}
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Your Qoute's</Form.Label>
-        <Form.Control
-          as="textarea"
-          rows={3}
-          placeholder="I am ..."
-          className="inputName"
-          value={quotes}
-          onChange={(e) => setQuotes(e.target.value)}
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={addQuotes}>
-        Create Quote's
-      </Button>
-    </Form>
+    <Fragment>
+      <Container>
+        <Row className="margin">
+          {width <= 900 ? (
+            <>
+              <Col md={{ span: 8, offset: 2 }}>
+                <Card className="cards">
+                  <Card.Body>
+                    <Form className="form-user" onSubmit={addQuotes}>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlInput1"
+                      >
+                        <Form.Label>User</Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="your name"
+                          className="inputName"
+                          value={nama}
+                          onChange={(e) => setNama(e.target.value)}
+                        />
+                      </Form.Group>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlTextarea1"
+                      >
+                        <Form.Label>Your Qoute's</Form.Label>
+                        <Form.Control
+                          as="textarea"
+                          rows={3}
+                          placeholder="I am ..."
+                          className="inputName"
+                          value={text}
+                          onChange={(e) => setText(e.target.value)}
+                        />
+                      </Form.Group>
+                      <Button variant="primary" type="submit">
+                        Create Quote's
+                      </Button>
+                    </Form>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </>
+          ) : (
+            <>
+              <Col md={{ span: 6, offset: 3 }}>
+                <Card className="cards">
+                  <Card.Body>
+                    <Form className="form-user" onSubmit={addQuotes}>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlInput1"
+                      >
+                        <Form.Label>User</Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="your name"
+                          className="inputName"
+                          value={nama}
+                          onChange={(e) => setNama(e.target.value)}
+                        />
+                      </Form.Group>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlTextarea1"
+                      >
+                        <Form.Label>Your Qoute's</Form.Label>
+                        <Form.Control
+                          as="textarea"
+                          rows={3}
+                          placeholder="I am ..."
+                          className="inputName"
+                          value={text}
+                          onChange={(e) => setText(e.target.value)}
+                        />
+                      </Form.Group>
+                      <Button variant="primary" type="submit">
+                        Create Quote's
+                      </Button>
+                    </Form>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </>
+          )}
+        </Row>
+      </Container>
+      <Container fluid>
+        <h2 className="mt-4 new">New Quote's</h2>
+        <hr />
+        <Row className="mt-4">
+          <Col>
+            <NewQuotes />
+          </Col>
+        </Row>
+        <hr />
+      </Container>
+      <Container fluid>
+        <Row className="mt-4">
+          <WithHeaderAndQuoteExample newData={Data} />
+        </Row>
+      </Container>
+      <Container fluid>
+        <h2 className="mt-4 new">Carousel Quote's</h2>
+        <Row>
+          <Col>
+            <Slider newData={Data} />
+          </Col>
+        </Row>
+      </Container>
+    </Fragment>
   );
 }
 
-export default TextControlsExample;
+export default FormAndQuotes;
---
+++ frontend/src/components/loaders/loader.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./loader.scss";
+function Loaders() {
+  return (
+    <div className="loading">
+      <div className="lds-dual-ring"></div>
+    </div>
+  );
+}
+export default Loaders;
